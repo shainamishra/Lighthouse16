1a2eb7347a920d3e60a9e0c34100754f95da7dbd
Daren Bartolucci <darentbartolucci@gmail.com>
2021-05-15 18:55:46 -0700

    Island South text added

--- src/scenes/IslandSouth.js
+++ src/scenes/IslandSouth.js
@@ -6,7 +6,7 @@ class IslandSouth extends Phaser.Scene {
     preload() {
         // images
         this.load.image('islandsouth', './assets/IslandSouth.PNG');
-
+        this.load.image('hitbox', './assets/HitBox2.png');
 
         // obstacles
 
@@ -47,11 +47,36 @@ class IslandSouth extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - 10, game.config.width, 10, 0x042630).setOrigin(0, 0);
         this.add.rectangle(0, 0, game.config.width, 10, 0x042630).setOrigin(0, 0);
         this.add.rectangle(game.config.width - 10, 0, 10, game.config.height, 0x042630).setOrigin(0, 0);
-      
+        
+        ////////////////////////////////////////////////////
+        this.graves = this.add.sprite(850, 250, 'hitbox');
+        this.graves.setDisplaySize(600, 400);
+        this.graves.setInteractive({
+            useHandCursor: true
+        });
+        this.graves.interText = this.add.text(borderUISize + borderPadding * 20, borderUISize + borderPadding * 2, 'Strange text instead of names');
+        this.graves.interText.setFontSize(50);
+        this.graves.interText.setVisible(false);
     }
 
     update() {
-        
+        if(textTimerGraves == 0){
+            // if click on lighthouse
+            this.graves.on('pointerdown', function (pointer){
+                this.interText.setVisible(true);
+                textTimerGraves = 1;
+            });
+        }
+
+        // text on screen
+        if(textTimerGraves > 0 && textTimerGraves < 150) {
+            textTimerGraves += 1;
+        } 
+        else if(textTimerGraves >= 150){
+            // hide text
+            this.graves.interText.setVisible(false);
+            textTimerGraves = 0;
+        }
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
         // scene change on keypress
 
