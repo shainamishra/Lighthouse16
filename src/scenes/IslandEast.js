2c5f8fd53f7e666ba2f85aba845419e430b38712
Daren Bartolucci <darentbartolucci@gmail.com>
2021-05-15 18:35:23 -0700

    Island East dialogue added

--- src/scenes/IslandEast.js
+++ src/scenes/IslandEast.js
@@ -6,7 +6,7 @@ class IslandEast extends Phaser.Scene {
     preload() {
         // images
         this.load.image('islandeast', './assets/IslandEast.PNG');
-
+        this.load.image('hitbox', './assets/HitBox2.png');
 
         // obstacles
 
@@ -48,11 +48,65 @@ class IslandEast extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - 10, game.config.width, 10, 0x042630).setOrigin(0, 0);
         this.add.rectangle(0, 0, game.config.width, 10, 0x042630).setOrigin(0, 0);
         this.add.rectangle(game.config.width - 10, 0, 10, game.config.height, 0x042630).setOrigin(0, 0);
-      
+        
+        //////////////////////////
+        this.fishingpole = this.add.sprite(400,300, 'hitbox');
+        this.fishingpole.setDisplaySize(300, 400);
+        this.fishingpole.setInteractive({
+            useHandCursor: true
+        });
+        this.fishingpole.interText = this.add.text(borderUISize + borderPadding * 20, borderUISize + borderPadding * 2, 'The Reel is missing');
+        this.fishingpole.interText.setFontSize(50);
+        this.fishingpole.interText.setVisible(false);
+
+        this.bucket = this.add.sprite(900,550, 'hitbox');
+        this.bucket.setDisplaySize(100, 150);
+        this.bucket.setInteractive({
+            useHandCursor: true
+        });
+        this.bucket.interText = this.add.text(borderUISize + borderPadding * 20, borderUISize + borderPadding * 2, 'There is a key in the bucket');
+        this.bucket.interText.setFontSize(50);
+        this.bucket.interText.setVisible(false);
     }
 
     update() {
 
+        if(textTimerRod== 0){
+            // if click on lighthouse
+            this.fishingpole.on('pointerdown', function (pointer){
+                this.interText.setVisible(true);
+                textTimerRod = 1;
+            });
+        }
+
+        // text on screen
+        if(textTimerRod > 0 && textTimerRod < 150) {
+            textTimerRod += 1;
+        } 
+        else if(textTimerRod >= 150){
+            // hide text
+            this.fishingpole.interText.setVisible(false);
+            textTimerRod = 0;
+        }
+
+        
+        if(textTimerBucket == 0){
+            // if click on lighthouse
+            this.bucket.on('pointerdown', function (pointer){
+                this.interText.setVisible(true);
+                textTimerBucket = 1;
+            });
+        }
+
+        // text on screen
+        if(textTimerBucket > 0 && textTimerBucket < 150) {
+            textTimerBucket += 1;
+        } 
+        else if(textTimerBucket >= 150){
+            // hide text
+            this.bucket.interText.setVisible(false);
+            textTimerBucket = 0;
+        }
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
         // scene change on keypress
 
