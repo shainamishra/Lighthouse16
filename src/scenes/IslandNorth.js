1428f466ac55b9c590f9731ab1f1806e850a6017
drewgra <60277180+drewgra@users.noreply.github.com>
2021-05-23 20:39:11 -0700

    caps

--- src/scenes/IslandNorth.js
+++ src/scenes/IslandNorth.js
@@ -5,7 +5,7 @@ class IslandNorth extends Phaser.Scene {
 
     preload() {
         // images
-        this.load.image('islandnorth', './assets/puzzle1/IslandNorth.PNG');
+        this.load.image('islandnorth', './assets/puzzle1/IslandNorth.png');
         this.load.image('hitbox', './assets/HitBox2.png');
         this.load.image('cellarUnlocked', './assets/puzzle1/overlays/cellarUnlocked.png');
 
