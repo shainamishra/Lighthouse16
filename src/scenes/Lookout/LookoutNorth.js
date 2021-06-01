1400f424bedcb3696b6463d7e24f4b8a9f53c7a6
“Shaina <smishra8@ucsc.edu>
2021-06-01 14:11:34 -0700

    playtestable

--- src/scenes/Lookout/LookoutNorth.js
+++ src/scenes/Lookout/LookoutNorth.js
@@ -841,10 +841,6 @@ class LookoutNorth extends Phaser.Scene {
         };
     
         // delete this
-        hatch = 1;
-        weights = 1;
-        citrine = 1;
-        balanced = 1;
 
         if(Phaser.Input.Keyboard.JustDown(keySPACE)){
             this.sound.get('lookout_music').stop();
