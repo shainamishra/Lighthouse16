e861610e58e7955e406c72f5a581ca036647716f
“Shaina <smishra8@ucsc.edu>
2021-06-01 20:24:22 -0700

    Instruction screen

--- src/scenes/Instruction.js
+++ src/scenes/Instruction.js
@@ -5,13 +5,13 @@ class Instruction extends Phaser.Scene {
     
     preload() {
         // images
-        this.load.image('bg', './assets/bg.png');
+        this.load.image('instructions', './assets/instructions.png');
     }
 
     create() {
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
         // place tile sprite
-        this.bg = this.add.tileSprite(0, 0, 1280, 720, 'bg').setOrigin(0, 0); 
+        this.bg = this.add.tileSprite(0, 0, 1280, 720, 'instructions').setOrigin(0, 0); 
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
         // change scenes
@@ -44,7 +44,5 @@ class Instruction extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - 10, game.config.width, 10, 0x042630).setOrigin(0, 0);
         this.add.rectangle(0, 0, game.config.width, 10, 0x042630).setOrigin(0, 0);
         this.add.rectangle(game.config.width - 10, 0, 10, game.config.height, 0x042630).setOrigin(0, 0);
-        this.add.text(borderUISize + borderPadding * 20, borderUISize + borderPadding * 10, 'WASD for movement').setFontSize(50);
-        this.add.text(borderUISize + borderPadding * 15, borderUISize + borderPadding * 20, 'Left click to interact').setFontSize(50);
     }
 }
