b5c67073f284e5b097705806a594a3b4a12f967b
Daren Bartolucci <darentbartolucci@gmail.com>
2021-06-06 01:38:31 -0700

    Intro beta

--- src/scenes/Intro.js
+++ src/scenes/Intro.js
@@ -12,7 +12,8 @@ class Intro extends Phaser.Scene {
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
         // place tile sprite
         this.bg = this.add.tileSprite(0, 0, 1280, 720, 'blkbg').setOrigin(0, 0); 
-
+        this.bg.intertext = this.add.text(borderUISize + borderPadding * 20 -340, borderUISize + borderPadding * 2 + 100, '        As I approached the island\nI glanced down at the strange wooden box\n    the old fortune teller had given me. ');
+        this.bg.intertext.setFontSize(50);
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
         // text fades in n out and when it gets to the end go to island north
         //text in the corner: press space to skip faster?
