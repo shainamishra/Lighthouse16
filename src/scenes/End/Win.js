ae83ebb9755e0285215f269cbf48de5d3580914e
“Shaina <smishra8@ucsc.edu>
2021-06-07 04:20:01 -0700

    reposition win text

--- src/scenes/End/Win.js
+++ src/scenes/End/Win.js
@@ -18,15 +18,15 @@ class Win extends Phaser.Scene {
         this.bg = this.add.tileSprite(0, 0, 1280, 720, 'winbg').setOrigin(0, 0); 
         this.credits = this.add.sprite(300, 360, 'hitbox');
         //this.credits.intertext = this.add.text(borderUISize + borderPadding * 20 + 200, borderUISize + borderPadding * 2 -40, '                   Programming\n                  Shaina Mishra\n                 Daren Bartolucci\n\n                       Art\n                   Rosie Longo\n                   Chantel Gee\n                  Shaina Mishra\n\n                      Music\n                   Rosie Longo\n\n                       SFX\n                 Daren Bartolucci\n                   Rosie Longo\n                  Shaina Mishra');
-        this.credits.intertext = this.add.text(450, 150, '                   Programming\n                  Shaina Mishra\n                 Daren Bartolucci\n                   Rosie Longo\n\n                       Art\n                   Chantel Gee\n                   Rosie Longo\n                  Shaina Mishra\n\n');
+        this.credits.intertext = this.add.text(450, 120, '                   Programming\n                  Shaina Mishra\n                 Daren Bartolucci\n                   Rosie Longo\n\n                       Art\n                   Chantel Gee\n                   Rosie Longo\n                  Shaina Mishra\n\n');
         this.credits.intertext.setFontSize(30);
         this.credits.intertext.setVisible(true);
-        this.credits.intertext2 = this.add.text(450, 150, '                      Music\n                   Rosie Longo\n\n\n\n                       SFX\n                 Daren Bartolucci\n                   Rosie Longo\n                  Shaina Mishra');
+        this.credits.intertext2 = this.add.text(450, 120, '                      Music\n                   Rosie Longo\n\n\n\n                       SFX\n                 Daren Bartolucci\n                   Rosie Longo\n                  Shaina Mishra');
         this.credits.intertext2.setFontSize(30);
         this.credits.intertext2.setVisible(false);
         
         this.sourcethanks = this.add.sprite(300, 360, 'hitbox');
-        this.sourcethanks.intertext = this.add.text(borderUISize + borderPadding * 20 + 260, borderUISize + borderPadding * 2 + 48, 'Ocean sfx for island music\nsourced from freesound by Noted451\nhttps://freesound.org/people/Noted451/\nsounds/531015/\n\nRainbow fire info\nhttps://sciencenotes.org/how-to-make-\ncolored-fire/\n\nFishing advice :)\nhttps://freshwaterfishingadvice.com\n/salt-bait-fishing/');
+        this.sourcethanks.intertext = this.add.text(borderUISize + borderPadding * 20 + 250, borderUISize + borderPadding * 2 + 48, 'Ocean sfx for island music\nsourced from freesound by Noted451\nhttps://freesound.org/people/Noted451/\nsounds/531015/\n\nRainbow fire info\nhttps://sciencenotes.org/how-to-make-\ncolored-fire/\n\nFishing advice :)\nhttps://freshwaterfishingadvice.com\n/salt-bait-fishing/');
         this.sourcethanks.intertext.setFontSize(25);
         this.sourcethanks.intertext.setVisible(false);
 
