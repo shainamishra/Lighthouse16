ad4c3e252f0e098a7a15791b20e9bd9e9f0de18f
Daren Bartolucci <darentbartolucci@gmail.com>
2021-06-06 23:37:44 -0700

    Credits work

--- src/scenes/End/Win.js
+++ src/scenes/End/Win.js
@@ -6,7 +6,7 @@ class Win extends Phaser.Scene {
     preload(){
         // images
         this.load.image('blkbg', './assets/goodend.png');
-
+        this.load.image('hitbox', './assets/HitBox2.png');
         // audio
     }
 
@@ -16,8 +16,12 @@ class Win extends Phaser.Scene {
         this.credits = this.add.sprite(300,360, 'hitbox');
         this.credits.intertext = this.add.text(borderUISize + borderPadding * 20 + 200, borderUISize + borderPadding * 2 -60, '                   Programming\n                      Shiana\n                      Daren\n\n                       Art\n                      Rose\n                     Chantel\n                     Shaina\n\n                      Music\n                      Rosie\n\n                       SFX\n                      Daren\n                      Rosie\n                      Shaina');
         this.credits.intertext.setFontSize(30);
-        this.credits.intertext.setAlpha(1);
-
+        this.credits.intertext.setVisible(true);
+        
+        this.sourcethanks = this.add.sprite(300,360, 'hitbox');
+        this.sourcethanks.intertext = this.add.text(borderUISize + borderPadding * 20 + 200, borderUISize + borderPadding * 2 + 30, 'Ocean sfx for island music\nsourced from freesound by Noted451\nhttps://freesound.org/people/Noted451/\nsounds/531015/\n\nRainbow fire info\nhttps://sciencenotes.org/how-to-make-\ncolored-fire/\n\nFishing advice :)\nhttps://freshwaterfishingadvice.com\n/salt-bait-fishing/');
+        this.sourcethanks.intertext.setFontSize(30);
+        this.sourcethanks.intertext.setVisible(false);
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
         // borders
         this.add.rectangle(0, 0, 10, game.config.height, 0x2c2b45).setOrigin(0, 0);
@@ -30,21 +34,28 @@ class Win extends Phaser.Scene {
 
 
 
-        this.introtexttimer = 0;
+        this.wintexttimer = 0;
     }
     
     update() {
         //yo this is from the intro it only works til the fifth one i think but anyway the frame work for fading text in n out and the timer is here..........
-        if(this.introtexttimer == 0 ){
-            this.introtexttimer +=1;
+        if(this.wintexttimer >= 0 && this.wintexttimer <500){
+            this.wintexttimer +=1;
 
         }
-       
-        else if (this.introtexttimer == 500){
-            this.scene.start("instructionScene"); 
+
+        else if (this.wintexttimer == 500){
+            this.wintexttimer +=1;
+            this.credits.intertext.setVisible(false);
+            this.sourcethanks.intertext.setVisible(true);
         }
-        
-    
+        else if(this.wintexttimer > 500 && this.wintexttimer < 999){
+            this.wintexttimer +=1;
+ 
+        }
+        else if(this.wintexttimer == 1000){
+            this.scene.start("instructionScene");
+        }
 
     }
 }
