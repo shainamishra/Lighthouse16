ce897cbf446213019ff96cac7322ef326c0c673e
Rosie <rclongo@ucsc.edu>
2021-06-07 00:15:48 -0700

    intro adjustments

--- src/scenes/Intro.js
+++ src/scenes/Intro.js
@@ -51,7 +51,9 @@ class Intro extends Phaser.Scene {
         this.ieight = this.add.sprite(300,360, 'hitbox');
         this.ieight.intertext = this.add.text(borderUISize + borderPadding * 20 -350, borderUISize + borderPadding * 2 + 200, '      And so to settle this, I arrive.');
         this.ieight.intertext.setFontSize(50);
-        this.ieight.intertext.setAlpha(0);    
+        this.ieight.intertext.setAlpha(0);
+        
+        this.spaceText = this.add.text(borderUISize + borderPadding * 30, borderUISize + borderPadding * 2 + 600, 'Press space to continue');
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
         // borders
         this.add.rectangle(0, 0, 10, game.config.height, 0x042630).setOrigin(0, 0);
@@ -98,11 +100,10 @@ class Intro extends Phaser.Scene {
                 nextText = this.textList[1];
             });
             }
-            else if(this.textList.length == 0){
+            else if(this.textList.length == 1){
                 this.sound.play("CrashingWaves");
                 this.scene.start("instructionScene"); 
             }
         }
     }
-
 }
