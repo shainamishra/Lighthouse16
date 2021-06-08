4fa8c20400d50582c7a44bdd6542ef98f6b758af
“Shaina <smishra8@ucsc.edu>
2021-06-07 21:43:22 -0700

    changed border color of clockMain

--- src/scenes/Main/Clock.js
+++ src/scenes/Main/Clock.js
@@ -43,10 +43,10 @@ class Clock extends Phaser.Scene {
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////
         // borders
-        this.add.rectangle(0, 0, 10, game.config.height, 0x2c2b45).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - 10, game.config.width, 10, 0x2c2b45).setOrigin(0, 0);
-        this.add.rectangle(0, 0, game.config.width, 10, 0x2c2b45).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - 10, 0, 10, game.config.height, 0x2c2b45).setOrigin(0, 0);
+        this.add.rectangle(0, 0, 10, game.config.height, 0x522729).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - 10, game.config.width, 10, 0x522729).setOrigin(0, 0);
+        this.add.rectangle(0, 0, game.config.width, 10, 0x522729).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - 10, 0, 10, game.config.height, 0x522729).setOrigin(0, 0);
     }
 
     update(){
