2b920697ab70eca6f52d296f76b42907e93d8243
“Shaina <smishra8@ucsc.edu>
2021-06-08 21:59:47 -0700

    test

--- src/scenes/Clown/ClownWest.js
+++ src/scenes/Clown/ClownWest.js
@@ -12,12 +12,7 @@ class ClownWest extends Phaser.Scene {
         // place tile sprite
         this.bg = this.add.tileSprite(0, 0, 1280, 720, 'daren').setOrigin(0, 0); 
 
-        /////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // borders
-        this.add.rectangle(0, 0, 10, game.config.height, 0x2c2b45).setOrigin(0, 0);
-        this.add.rectangle(0, game.config.height - 10, game.config.width, 10, 0x2c2b45).setOrigin(0, 0);
-        this.add.rectangle(0, 0, game.config.width, 10, 0x2c2b45).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - 10, 0, 10, game.config.height, 0x2c2b45).setOrigin(0, 0);
+        /////////////////////////////////////////////////////////////////////////////////////
 
         //define keys
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
